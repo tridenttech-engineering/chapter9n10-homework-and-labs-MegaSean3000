05f78155eadb58c0ee7b822e0608242a3b1a422f
MegaSean3000 <sfp1216@gmail.com>
2025-03-17 18:50:38 -0400

    Update hw10-13.cpp

--- hw10-13.cpp
+++ hw10-13.cpp
@@ -1,17 +1,13 @@
-//Lab 9-2.cpp - displays two monthly car payments
-//Created/revised by <your name> on <current date>
-
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 using namespace std;
 
-//function prototype
-double getPayment(int, double, int);
+// Function prototype (changed return type to void)
+void getPayment(int, double, int, double &);
 
 int main()
 {
-    //declare variables
     int carPrice = 0;
     int rebate = 0;
     double creditRate = 0.0;
@@ -19,7 +15,10 @@ int main()
     int term = 0;
     double creditPayment = 0.0;
     double dealerPayment = 0.0;
+    double totalCredit = 0.0;
+    double totalDealer = 0.0;
 
+    // Input values from the user
     cout << "Car price (after any trade-in): ";
     cin >> carPrice;
     cout << "Rebate: ";
@@ -31,30 +30,12 @@ int main()
     cout << "Term in years: ";
     cin >> term;
 
-    //call function to calculate payments
-    creditPayment = getPayment(carPrice - rebate,
-        creditRate / 12, term * 12);
-    dealerPayment = getPayment(carPrice, 
-        dealerRate / 12, term * 12);    //assign values to calculate payments
-    
-    //display payments
-    cout << fixed << setprecision(2) << endl; 
-    cout << "Credit union payment: $" 
-        << creditPayment << endl;
-    cout << "Dealer payment: $"
-        << dealerPayment << endl;
-    
-    return 0;
-}//end of main function    
+    // Call function to calculate payments (no return value)
+    getPayment(carPrice - rebate, creditRate / 12, term * 12, creditPayment);
+    getPayment(carPrice, dealerRate / 12, term * 12, dealerPayment);
 
-    //*****function definitions*****
-double getPayment(int prin,
-                  double monthRate, 
-                  int months)
-{
-    //calculates and returns a monthly payment
-    double monthPay = 0.0;
-    monthPay = prin * monthRate / 
-        (1 - pow(monthRate + 1, -months));
-    return monthPay;
-} //end of getPayment function//*****function definition*****
+    // Display the monthly payments
+    cout << fixed << setprecision(2) << endl;
+    if (creditPayment == -1 || dealerPayment == -1) {
+        cout <<
+
